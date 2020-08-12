222b680c180c2b4ff6365b1a469438c477cbb4ad
Fabien <fabien.webdev@gmail.com>
2020-08-12 17:57:48 +0900

    update CLI help text (boilerplate)

--- src/meow.js
+++ src/meow.js
@@ -9,6 +9,7 @@ const help = meow(`
   ${chalk.red('Options:')}
       ${chalk('--name, -n  Specify the name of the component|page|action&reducer (required except for boilerplate command)')}
       ${chalk('--folder, -f  Specify a folder to save the component|page|action&reducer (optional, does not work for boilerplate command)')}
+      ${chalk('--default, -d  Use default settings instead of using the prompt questions (optional, work only for the boilerplate command)')}
 
   ${chalk.yellow('Commands:')}
         ${chalk.white('memo              create a new memo component')}
@@ -17,7 +18,7 @@ const help = meow(`
         ${chalk.white('pcp               create a new pure component')}
         ${chalk.white('page              create a new page')}
         ${chalk.white('rdx               add a new set of action & reducer')}
-        ${chalk.gray('boilerplate        add a new full project boilerplate code')}
+        ${chalk.white('boilerplate        add a new full project boilerplate code')}
   `, {
   boolean: constants.COMMAND_LIST,
   flags: {
@@ -35,6 +36,11 @@ const help = meow(`
       type: 'string',
       alias: 's',
       default: 'styled'
+    },
+    default: {
+      type: 'boolean',
+      alias: 'd',
+      default: false
     }
   }
 })
