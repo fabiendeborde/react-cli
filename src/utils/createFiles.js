07705a46d8f08b46be50ca252fb7646bba782179
Fabien <fabien.webdev@gmail.com>
2020-08-12 10:28:43 +0900

    add create package, jsx, style, story, test, cypress test helpers

--- src/utils/createFiles.js
+++ src/utils/createFiles.js
@@ -2,7 +2,12 @@ const fs = require('fs')
 const chalk = require('chalk')
 const currentDir = process.cwd()
 
-module.exports = (fileName, content, path) => {
+const packageJson = require('../templates/packageJson')
+const style = require('../templates/style')
+const stories = require('../templates/stories')
+const cypressSample = require('../templates/cypressSample')
+
+const createFile = (fileName, content, path) => {
   const fullPath = `${currentDir}${path}`
   try {
     // check if at the root of the project
@@ -29,4 +34,67 @@ module.exports = (fileName, content, path) => {
   } catch (error) {
     console.warn(chalk.red(`Oops! An error occured while trying to create ${fileName}...`))
   }
+}
+
+const createPackage = (name, dir) => {
+  createFile('package.json', packageJson(name), dir)
+}
+const createJSX = (name, dir, options) => {
+  let jsx
+  switch (options.input) {
+    case 'memo':
+      jsx = require('../templates/memo')
+      break
+    case 'fn':
+      jsx = require('../templates/functionalComponent')
+      break
+    case 'cp':
+      jsx = require('../templates/component')
+      break
+    case 'pcp':
+      jsx = require('../templates/pureComponent')
+      break
+    case 'page':
+      jsx = require('../templates/page')
+      break
+    default:
+      break
+  }
+  if (jsx) createFile(`${name}.jsx`, jsx(name, options), dir)
+  // console.log('options', options)
+  // console.log('jsx', jsx)
+}
+const createStyle = (name, dir, styleOption) => {
+  switch (styleOption) {
+    case 'css':
+      createFile(`${name}.css`, '', dir)
+      break
+    case 'scss':
+      createFile(`${name}.scss`, '', dir)
+      break
+    case 'sass':
+      createFile(`${name}.sass`, '', dir)
+      break
+    default:
+      createFile(`${name}.style.js`, style(name), dir)
+      break
+  }
+}
+const createStory = (name, dir) => {
+  createFile(`${name}.stories.js`, stories(name), dir)
+}
+const createTest = (name, dir) => {
+  createFile(`${name}.test.js`, '', dir)
+}
+const createCypress = (name, dir) => {
+  createFile(`${name}.spec.js`, cypressSample(name), dir)
+}
+
+module.exports = {
+  createPackage,
+  createJSX,
+  createStyle,
+  createStory,
+  createTest,
+  createCypress
 }
