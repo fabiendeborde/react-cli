02c356ec3c7a6d3406ebb1e15587478e0d47c446
Fabien <fabien.webdev@gmail.com>
2021-08-12 13:52:52 +0200

    update redux script

--- src/scripts/addActionReducer.js
+++ src/scripts/addActionReducer.js
@@ -2,7 +2,9 @@ const DIRECTORIES = require('../constants').DIRECTORIES
 
 const {
   createAction,
-  createReducer
+  createReducer,
+  createSelector,
+  createTypes
 } = require('../utils/createFiles')
 
 module.exports = (options) => {
@@ -10,11 +12,17 @@ module.exports = (options) => {
   const { name, folder } = options
   let actionDir = `${DIRECTORIES.actions}`
   let reducerDir = `${DIRECTORIES.reducers}`
+  let selectorDir = `${DIRECTORIES.selectors}`
+  let typeDir = `${DIRECTORIES.types}`
   if (options.folder) {
-    actionDir = `${DIRECTORIES.actions}${folder}/${name}`
-    reducerDir = `${DIRECTORIES.reducers}${folder}/${name}`
+    actionDir = `${DIRECTORIES.actions}${folder}/`
+    reducerDir = `${DIRECTORIES.reducers}${folder}/`
+    selectorDir = `${DIRECTORIES.selectors}${folder}/`
+    typeDir = `${DIRECTORIES.types}${folder}/`
   }
 
-  createAction(name, actionDir)
-  createReducer(name, reducerDir)
+  createAction({ name, dir: actionDir })
+  createReducer({ name, dir: reducerDir })
+  createSelector({ name, dir: selectorDir })
+  createTypes({ name, dir: typeDir })
 }
