148a811a4a0f38bb81209fb71bebf1ed204f2be6
Fabien <fabien.webdev@gmail.com>
2021-08-12 13:53:10 +0200

    update component script

--- src/scripts/addComponent.js
+++ src/scripts/addComponent.js
@@ -1,23 +1,26 @@
 const capitalize = require('../utils/capitalize')
-const DIRECTORIES = require('../constants').DIRECTORIES
 
 const {
   createPackage,
-  createJSX,
+  createComponent,
   createStyle,
-  createStory,
   createTest
 } = require('../utils/createFiles')
 
-module.exports = (options) => {
-  // console.log('Add Pure Component', options)
+module.exports = (options, directory) => {
+  console.log('Add Component', options)
   const name = capitalize(options.name)
-  let dir = `${DIRECTORIES.component}${name}`
-  if (options.folder) dir = `${DIRECTORIES.component}${options.folder}/${name}`
+  const { memo, javascript } = options
+  let dir = `${directory}${name}`
+  if (options.folder) dir = `${directory}${options.folder}/${name}`
 
-  createPackage(name, dir)
-  createJSX(name, dir, options)
-  createStyle(name, dir, options.style)
-  createStory(name, dir)
-  createTest(name, dir)
+  createPackage({ name, dir, javascript })
+  createComponent({
+    name,
+    dir,
+    memo,
+    javascript
+  })
+  createStyle({ name, dir, javascript })
+  createTest({ name, dir, javascript })
 }
